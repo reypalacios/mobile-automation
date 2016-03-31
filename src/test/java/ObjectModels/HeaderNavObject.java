a927c8427860e38b9b31d3748ab22f2ef03b620c
khadijaislam <khdja.lm@gmail.com>
2016-03-31 14:55:16 -0400

    add file

--- src/test/java/ObjectModels/HeaderNavObject.java
+++ src/test/java/ObjectModels/HeaderNavObject.java
@@ -1,4 +1,4 @@
-package objectModels;
+package ObjectModels;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
@@ -11,25 +11,38 @@ import org.openqa.selenium.support.PageFactory;
 /**
  * Created by rpalacios on 12/11/15.
  */
-public class HamburgerMenu {
+public class HeaderNavObject {
 
     Logger logger = Logger.getLogger(this.getClass());
 
     @AndroidFindBy(id = "android:id/up")
     @iOSFindBy(name = "EasyMenu Icon")
-    public static WebElement icon;
+    public static WebElement HamburgerMenu;
+    @iOSFindBy(name = "EasySearch Icon")
+    public static WebElement Search;
 
 
-    public HamburgerMenu(AppiumDriver<WebElement> wd) {
+    public HeaderNavObject(AppiumDriver<WebElement> wd) {
         PageFactory.initElements(new AppiumFieldDecorator(wd), this);
     }
 
     public void clickHamburger() {
         //wd.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[1]/android.view.View[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.ImageView[1]")).click();
 
-            icon.click();
-            logger.info("Click Hamburger Icon");
+        HamburgerMenu.click();
+        logger.info("Click Hamburger Icon");
+
+
+    }
+
+    public void ClickSearch() {
+        //wd.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[1]/android.view.View[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.ImageView[1]")).click();
+
+      Search.click();
+        logger.info("Click Search Icon");
 
 
     }
 }
+
+
