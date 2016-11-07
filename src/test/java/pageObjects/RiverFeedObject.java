3dc934bab8f0d29da93fd8baf490e876fee76a1f
reydavidpa <reydavid_x@hotmail.com>
2016-11-07 14:11:51 -0500

    add chromdriver to project

--- src/test/java/pageObjects/RiverFeedObject.java
+++ src/test/java/pageObjects/RiverFeedObject.java
@@ -22,11 +22,44 @@ public class RiverFeedObject {
     @iOSFindBy(xpath = "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")
     public static WebElement topPost;
 
-    @iOSFindBy(xpath = "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell/UIAStaticText[@visible='true']")
-    public static List<WebElement> visiblePosts;
+    @iOSFindBy(accessibility = "cellHeadlineTextView")
+    public static List<WebElement> headlines;
 
-    @iOSFindBy(xpath = "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[@visible='false']")
-    public static List<WebElement> notVisiblePosts;
+    @iOSFindBy(accessibility = "cellImageView")
+    public static List<WebElement> images;
+
+    @iOSFindBy(accessibility = "cellAuthorView")
+    public static List<WebElement> authors;
+
+    @iOSFindBy(accessibility = "cellTimeAgedView")
+    public static List<WebElement> timeageds;
+
+    @iOSFindBy(accessibility = "brandingColorView")
+    public static List<WebElement> brandingcolors;
+
+    @iOSFindBy(accessibility = "cellFlameView")
+    public static List<WebElement> flames;
+
+    @iOSFindBy(accessibility = "cellNumOfViews")
+    public static List<WebElement> numofviews;
+
+    @iOSFindBy(accessibility = "cellTimeViewsContainer")
+    public static List<WebElement> timeviewscontainers;
+
+    @iOSFindBy(accessibility = "cellTimeIcon")
+    public static List<WebElement> timeicons;
+
+    @iOSFindBy(accessibility = "cellAging")
+    public static List<WebElement> agings;
+
+    @iOSFindBy(accessibility = "cellCommentsIcon")
+    public static List<WebElement> commentsicons;
+
+    @iOSFindBy(accessibility = "cellNumOfComments")
+    public static List<WebElement> numofcomments;
+
+    @iOSFindBy(accessibility = "cellSearchSnippet")
+    public static List<WebElement> searchsnippets;
 
     public RiverFeedObject() {
         PageFactory.initElements(new AppiumFieldDecorator(App.driver), this);
